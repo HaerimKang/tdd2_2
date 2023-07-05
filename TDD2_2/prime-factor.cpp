cc53ae122656e214f56e208b5275437fd200b533
hr_kang <hyerim513@gmail.com>
2023-07-05 15:28:20 +0900

    [feautre] 48page 05/07

--- TDD2_2/prime-factor.cpp
+++ TDD2_2/prime-factor.cpp
@@ -17,9 +17,15 @@ public:
 					number /= 2;
 				}
 			}
+			else if(number == 6)
+			{
+				result.push_back(2);
+				result.push_back(3);
+			}
 			else
-
-			result.push_back(number);
+			{
+				result.push_back(number);
+			}
 		}
 		return result;
 	}
